8af14fb3b3a2f4e0074d41c8bef40a4780fece8a
Elyassaa <mbyassaa@gmail.com>
2025-09-18 14:13:01 +0100

    First commit - newbabyBag

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function displayList(){
+    const list = document.getElementById("menu");
+    const icon = document.getElementById("");
+    if(list.style.display==="none" || list.style.display===""){
+        list.style.display = "flex";
+    }else {
+
+        list.style.display ="none";
+    }
+}
